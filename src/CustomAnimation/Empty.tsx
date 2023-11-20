24a0898b4627205197b3f45f8ec6da24fa619974
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-11-20 21:52:25 +0100

    Add base for custom animation

--- src/CustomAnimation/Empty.tsx
+++ src/CustomAnimation/Empty.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, View } from 'react-native';
+import Animated from 'react-native-reanimated';
 
 export default function CustomAnimationExample() {
-  return <Text>CustomAnimationExample</Text>;
+
+  return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Button title="Run custom Animation" onPress={() => {
+
+    }} />
+    <Animated.View 
+      style={[
+        { 
+          backgroundColor: 'red', 
+          height: 100, 
+          width: 100,
+        },
+      ]} 
+    />
+  </View>;
 }
